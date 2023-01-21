3b6b7bbda1d74c130c52c1117a455053777af36c
freddo <fsolisvi@gmail.com>
2023-01-20 22:30:38 -0500

    add web server con fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app=FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3,4,5]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1> Hola soy la pagina de LRP"</h1>
+        <p>Soy el parrafo de la LRP</p>             
+    """
 
 def run():
     store.get_categories()
